e3b68e7928bdd6c202f3fca33c42eef80dd949ae
Christian Rodriguez <crodriguezochoa@ucdavis.edu>
2023-05-27 16:13:49 -0700

    updated addToCartHandler to account for item quantity

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,50 +6,66 @@ import Title from '../components/Title';
 import drinks from '../data/drinks.json';
 
 function HomeScreen({ route, navigation }) {
-    const [cart, setCart] = useState([]);
-    const [drinksData, setDrinksData] = useState(drinks.data);
+	const [cart, setCart] = useState([]);
+	const [drinksData, setDrinksData] = useState(drinks.data);
   
-    function addToCartHandler(drinkItem) {
-      setCart( (prevCartState) => [...prevCartState, drinkItem] );
-    }
+	function addToCartHandler(drinkItem) {
+		setCart( (prevCartState) => {
+			const isItemInCart = prevCartState.some(item => item.id === drinkItem.id)
+			if (isItemInCart) {
+				const updatedCart = prevCartState.map(item => {
+					if (item.id === drinkItem.id ) {
+						return { ...item, quantity: item.quantity + 1 }
+					}
+					else {
+						return item
+					}
+				})
+				return updatedCart
+			}
+			else {
+				[...prevCartState, {...drinkItem, quantity: 1}]
+			}
+		});
+	}
 
-    function removeFromCartHandler(drinkItem) {
-        setCart((prevCartState) => {
-            return prevCartState.filter((item) => item !== drinkItem)
-        })
-    }
+	function removeFromCartHandler(drinkItem) {
+		setCart((prevCartState) => {
+			return prevCartState.filter((item) => item !== drinkItem)
+		})
+	}
 
-    return (
-        <View style={styles.rootContainer}>
-            <Title>Drinks Cafe Menu</Title>
-            <PrimaryButton 
-                onPressBtn={() =>
-                    navigation.navigate('Cart', {
-                        onRemoveFromCart: removeFromCartHandler,
-                        cartDrinks: cart
-                    })
-                }>
-                View your shopping cart
-            </PrimaryButton>
+	return (
+		<View style={styles.rootContainer}>
+			<Title>Drinks Cafe Menu</Title>
+			<PrimaryButton 
+				onPressBtn={() =>
+					navigation.navigate('Cart', {
+						onRemoveFromCart: removeFromCartHandler,
+						cartDrinks: cart
+					})
+				}>
+				View your shopping cart
+			</PrimaryButton>
 
-            <ScrollView>
-            {
-                drinksData &&
-                drinksData.map( (drinkInfo) => <DrinkCard key={drinkInfo.id} drink={drinkInfo} routeName={route.name} onAddToCart={addToCartHandler} />)
-            }
-            </ScrollView>
+			<ScrollView>
+			{
+				drinksData &&
+				drinksData.map( (drinkInfo) => <DrinkCard key={drinkInfo.id} drink={drinkInfo} routeName={route.name} onAddToCart={addToCartHandler} />)
+			}
+			</ScrollView>
 
-        </View>
-    )
+		</View>
+	)
 }
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-    rootContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#038767',
-    }
+	rootContainer: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#038767',
+	}
   });
