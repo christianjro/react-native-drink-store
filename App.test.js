1800b2591e89699b99b1195a1b5fa0b2e25d4c2d
Christian Rodriguez <crodriguezochoa@ucdavis.edu>
2023-07-21 17:17:52 -0700

    added test to verify multple drink types and quantities

--- App.test.js
+++ App.test.js
@@ -22,7 +22,7 @@ describe('<App />', () => {
 
     // aim for the addToCartButton and press it
     const drinkName = 'Modern Plastic Computer Tea'
-    const drinkCard = getByTestId(drinkName).parent
+    const drinkCard = getByTestId(drinkName)
     const addToCartButton = within(drinkCard).getByText('Add to Cart')
 
     fireEvent.press(addToCartButton)
@@ -32,7 +32,7 @@ describe('<App />', () => {
     fireEvent.press(cartLink)
 
     // aim for the added drink (null if it doesn't exist)
-    const cartContainer = getByTestId('cartContainer').parent
+    const cartContainer = getByTestId('cartContainer')
     const addedDrink = within(cartContainer).queryByText(drinkName)
 
     // finalize test
@@ -45,7 +45,7 @@ describe('<App />', () => {
 
     // aim for the addToCartButton and press it 3 times
     const drinkName = 'Modern Plastic Computer Tea'
-    const drinkCard = getByTestId(drinkName).parent
+    const drinkCard = getByTestId(drinkName)
     const addToCartButton = within(drinkCard).getByText('Add to Cart')
 
     fireEvent.press(addToCartButton)
@@ -57,11 +57,49 @@ describe('<App />', () => {
     fireEvent.press(cartLink)
 
     // aim for the added drink (null if it doesn't exist)
-    const cartContainer = getByTestId('cartContainer').parent
+    const cartContainer = getByTestId('cartContainer')
     const drinkQuantity = within(cartContainer).getByTestId('quantity')
     const drinkQuantityText = drinkQuantity.props.children.join('')
 
     expect(drinkQuantityText).toBe('Quantity: 3')
   })
 
+  it('can add multiple drink types to the cart', () => {
+    const { getByText, getByTestId } = render(<App />)
+
+    // add first drink to cart
+    const drinkNameA = 'Modern Plastic Computer Tea'
+    const drinkCardA = getByTestId(drinkNameA)
+    const addToCartButtonA = within(drinkCardA).getByText('Add to Cart')
+
+    fireEvent.press(addToCartButtonA)
+    fireEvent.press(addToCartButtonA)
+
+    // add second drink to cart
+    const drinkNameB = 'Handcrafted Rubber Towels Smoothie'
+    const drinkCardB = getByTestId(drinkNameB)
+    const addToCartButtonB = within(drinkCardB).getByText('Add to Cart')
+
+    fireEvent.press(addToCartButtonB)
+
+    // navigate to the cart
+    const cartLink = getByText('View your shopping cart')
+    fireEvent.press(cartLink)
+
+    // check first drink
+    const cartContainer = getByTestId('cartContainer')
+    const drinkContainerA = within(cartContainer).getByTestId(drinkNameA)
+    const drinkQuantityA = within(drinkContainerA).getByTestId('quantity')
+    const drinkQuantityTextA = drinkQuantityA.props.children.join('')
+
+    expect(drinkQuantityTextA).toBe('Quantity: 2')
+
+    // check second drink
+
+    const drinkContainerB = within(cartContainer).getByTestId(drinkNameB)
+    const drinkQuantityB = within(drinkContainerB).getByTestId('quantity')
+    const drinkQuantityTextB = drinkQuantityB.props.children.join('')
+
+    expect(drinkQuantityTextB).toBe('Quantity: 1')
+  })
 });
